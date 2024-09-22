9e28ccf9cadb46648b07d6cb66c7d432ed39cf67
Delia Casmej <deliacasmej@gmail.com>
2024-09-21 21:37:03 -0500

    update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
   const items = ["New York", "London", "Tokyo", "Paris", "Chicago"];
 
+  const message = items.length === 0 ? <p>No items found</p> : null;
+
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li>{item}</li>
