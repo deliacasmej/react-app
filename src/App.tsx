63b9baed99ce4ffbb2e8d7eaf13f71f7edf977dc
Delia Casmej <deliacasmej@gmail.com>
2024-09-21 22:19:39 -0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import ListGroup from './components/ListGroup';
 
 
 function App() {
-  return <div><ListGroup /></div>
+
+
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return <div><ListGroup{items} heading="Cities" /></div>;
+
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,31 @@
-function ListGroup() {
-  const items = ["New York", "London", "Tokyo", "Paris", "Chicago"];
+import { useState } from "react";
 
-  const message = items.length === 0 ? <p>No items found</p> : null;
+interface Props {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
-      {message}
+      <h1>[{heading}]</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
+            key={item}
+            onClick=
+            {() => {
+              setSelectedIndex(index);
+            }}
+          </li>
         ))}
       </ul>
     </>
